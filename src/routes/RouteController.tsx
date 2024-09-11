553bd05717119f4ce439a1f07c3a315fa8a2291a
Ilhom <khaitovilhom04@gmail.com>
2024-09-11 08:35:18 +0500

    recipes

--- src/routes/RouteController.tsx
+++ src/routes/RouteController.tsx
@@ -2,8 +2,8 @@ import {useRoutes} from "react-router-dom";
 import {SuspenseElement as Suspense} from "../utils";
 import React, {LazyExoticComponent} from "react";
 
-const Home : LazyExoticComponent<any> = React.lazy(() => import("./home/Home.tsx"))
-const Details : LazyExoticComponent<any> = React.lazy(() => import("./details/Details.tsx"))
+const Home: LazyExoticComponent<any> = React.lazy(() => import("./home/Home.tsx"))
+const Details: LazyExoticComponent<any> = React.lazy(() => import("./details/Details.tsx"))
 const Liked: LazyExoticComponent<any> = React.lazy(() => import("./liked/Liked.tsx"))
 
 const RouteController = () => {
